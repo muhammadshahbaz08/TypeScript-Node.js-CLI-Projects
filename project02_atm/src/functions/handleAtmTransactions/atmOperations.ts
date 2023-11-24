b55046665c69d0b4bfdfe1feaaffaeba279ea39c
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2023-11-24 12:55:01 +0500

    used await keyword , to avoid syncronous behavior of withdrawl function in Loop

--- project02_atm/src/functions/handleAtmTransactions/atmOperations.ts
+++ project02_atm/src/functions/handleAtmTransactions/atmOperations.ts
@@ -5,20 +5,22 @@ import { WithDrawCash } from "./withDrawCash.js";
 import { balanceInquiry } from "./balanceInquiry.js";
 
 export const atmOperations = async (user: User) => {
-  const res = await inquirer.prompt({
-    type: "list",
-    name: "select",
-    message: "Choose the Following Operations :",
-    choices: ["WithDraw", "BalanceInquiry", "exit"],
-  });
+  let continueTransaction = true;
+  while (continueTransaction) {
+    const res = await inquirer.prompt({
+      type: "list",
+      name: "select",
+      message: "Choose the Following Operations :",
+      choices: ["WithDraw", "BalanceInquiry", "exit"],
+    });
 
-  if (res.select === "WithDraw") {
-    WithDrawCash(user);
-  }
-  if (res.select === "BalanceInquiry") {
-    balanceInquiry(user);
-  }
-  if (res.select === "exit") {
-    console.log(`Thanks For Using ATM`);
+    if (res.select === "WithDraw") {
+      await WithDrawCash(user);
+    } else if (res.select === "BalanceInquiry") {
+      await balanceInquiry(user);
+    } else if (res.select === "exit") {
+      console.log(`Thanks For Using ATM`);
+      continueTransaction = false;
+    }
   }
 };
