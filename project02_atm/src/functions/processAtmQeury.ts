6969d5ffbf4578d817d922ebb2f3036e58c25925
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2023-11-24 12:56:21 +0500

    Remove Unnecessaty Indentaions , & add Comments

--- project02_atm/src/functions/processAtmQeury.ts
+++ project02_atm/src/functions/processAtmQeury.ts
@@ -1,17 +1,18 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
 import { User } from "../interface/interface.js";
-
 import { atmOperations } from "./handleAtmTransactions/atmOperations.js";
 
 export const processAtmQeury = async (users: User[]) => {
+  // Intro Line
+  console.log(`${chalk.bgMagentaBright.bold("Project 02: ATM CLI.")}`);
   const res = await inquirer.prompt({
     type: "number",
     message: "Enter PIN CODE :",
     name: "pin",
   });
-
+  //Pin Validation
   const user = users.find((val) => val.pin === res.pin);
-
   if (user) {
     console.log(`Welcome ${user.name}`);
     atmOperations(user); //passing the user Data to atm Operation Module.
