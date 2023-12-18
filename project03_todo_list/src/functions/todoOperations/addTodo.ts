8ab1a47b94fc30122d023d82b9ce477cac70f990
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2023-12-18 16:49:31 +0500

    feat(addTodo): implement function to add todos

---
+++ project03_todo_list/src/functions/todoOperations/addTodo.ts
@@ -0,0 +1,28 @@
+import { Todo } from "../../utils/todoType.js";
+import inquirer from "inquirer";
+
+export const addTodo = async (arr: Todo[]) => {
+  const res = await inquirer.prompt([
+    {
+      type: "input",
+      name: "task",
+      message: "Enter your Task : ",
+    },
+    {
+      type: "input",
+      name: "dueDate",
+      message: "Enter the due date (YYYY-MM-DD):",
+      validate: (value) =>
+        !isNaN(Date.parse(value)) || "Please enter a valid date",
+      filter: (value) => new Date(value),
+    },
+    {
+      type: "list",
+      name: "status",
+      message: "Select Task Status :",
+      choices: ["incomplete", "completed", "inprogress"],
+    },
+  ]);
+
+  arr.push(res);
+};
