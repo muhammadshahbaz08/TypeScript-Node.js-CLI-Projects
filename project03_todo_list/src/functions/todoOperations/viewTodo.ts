38ab8a5b6f391801556be213bc82a9821b1b0df5
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2023-12-25 10:38:41 +0500

    [Added TODO ID] Colum to View Module.

--- project03_todo_list/src/functions/todoOperations/viewTodo.ts
+++ project03_todo_list/src/functions/todoOperations/viewTodo.ts
@@ -3,14 +3,13 @@ import { Todo } from "../../utils/todoType.js";
 import dayjs from "dayjs";
 export const viewTodo = (arr: Todo[]) => {
   let table = new Table({
-    head: ["Task", "Due Date", "Status"],
-    colWidths: [20, 20, 15],
+    head: ["Task ID", "Task", "Due Date", "Status"],
+    colWidths: [20, 20, 20, 15],
   });
   // Add each todo to the table
-  arr.forEach((todo: Todo) => {
+  arr.forEach((todo: Todo, index) => {
     let formattedDate = dayjs(todo.dueDate).format("YYYY-MM-DD");
-    table.push([todo.task, formattedDate, todo.status]);
+    table.push([index + 1, todo.task, formattedDate, todo.status]);
   });
-
   console.log(table.toString());
 };
