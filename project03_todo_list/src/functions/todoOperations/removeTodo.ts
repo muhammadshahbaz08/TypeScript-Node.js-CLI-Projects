ffb93e510867ea761b2c33028a995c76be34aa4a
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2023-12-31 10:34:07 +0500

    Add check for empty todo list before Removing Todo

--- project03_todo_list/src/functions/todoOperations/removeTodo.ts
+++ project03_todo_list/src/functions/todoOperations/removeTodo.ts
@@ -2,15 +2,22 @@ import inquirer from "inquirer";
 import { Todo } from "../../utils/todoType.js";
 import chalk from "chalk";
 export const removeTodo = async (arr: Todo[]) => {
-  const res = await inquirer.prompt({
-    type: "list",
-    name: "task",
-    message: "Select the Task to Remove :",
-    choices: arr.map((item, index) => ({ name: item.task, value: index })),
-  });
+  if (arr.length === 0) {
+    console.log(
+      chalk.red.bold(
+        "No, Todo's Available to Remove, Please Add Some Todo's first!"
+      )
+    );
+  } else {
+    const res = await inquirer.prompt({
+      type: "list",
+      name: "task",
+      message: "Select the Task to Remove :",
+      choices: arr.map((item, index) => ({ name: item.task, value: index })),
+    });
 
-  console.log(chalk.red.bold(`TASK REMOVED .`));
-  arr.splice(res.task, 1);
+    console.log(chalk.red.bold(`TASK REMOVED .`));
+    arr.splice(res.task, 1);
+  }
   console.log(`--------------------------------`);
 };
-// console.log(chalk.greenBright(`Invalid Todo ID `));
