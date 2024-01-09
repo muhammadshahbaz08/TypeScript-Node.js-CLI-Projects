8dd8abd5473fee6bdbb608249c77bc09faf3c132
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-01-09 12:24:29 +0500

    Update exit message in CLI app

--- project03_todo_list/src/functions/todoOperations/end.ts
+++ project03_todo_list/src/functions/todoOperations/end.ts
@@ -1,5 +1,9 @@
 import chalk from "chalk";
 
 export const end = () => {
-  console.log(chalk.red.bold(`Exiting!.`));
+  console.log(
+    chalk.red.bold(`       
+           PROGRAM ENDED
+           `)
+  );
 };
