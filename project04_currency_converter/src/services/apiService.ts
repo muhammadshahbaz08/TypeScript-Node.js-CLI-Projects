e8c2a3abf028936d1fc4bfa71115a70e10166efe
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-01-13 12:19:00 +0500

    Made "PKR" Base Currency

--- project04_currency_converter/src/services/apiService.ts
+++ project04_currency_converter/src/services/apiService.ts
@@ -1,6 +1,6 @@
 export const apiService = async () => {
   let fetchData = await fetch(
-    `https://v6.exchangerate-api.com/v6/a88ce2c3266454ca55bb4d59/latest/USD`
+    `https://v6.exchangerate-api.com/v6/a88ce2c3266454ca55bb4d59/latest/PKR`
   );
   let res = await fetchData.json();
   return res.conversion_rates;
