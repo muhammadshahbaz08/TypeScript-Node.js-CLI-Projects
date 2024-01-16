e482ce89a6f2649bb492545a118a491972159096
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-01-16 06:01:42 +0500

    📦 NEW: Added Chalk & inquirer Library

--- project04_currency_converter/src/services/apiService.ts
+++ project04_currency_converter/src/services/apiService.ts
@@ -1,6 +1,6 @@
 export const apiService = async () => {
   let fetchData = await fetch(
-    `https://v6.exchangerate-api.com/v6/a88ce2c3266454ca55bb4d59/latest/PKR`
+    `https://v6.exchangerate-api.com/v6/3c85c0bd193a7a0f863e07fb/latest/PKR`
   );
   let res = await fetchData.json();
   return res.conversion_rates;
--- project04_currency_converter/src/utils/cliInterface.ts
+++ project04_currency_converter/src/utils/cliInterface.ts
@@ -1,5 +1,32 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
 import { apiService } from "../services/apiService.js";
 
 export const cliInterface = async () => {
-  console.log(await apiService());
+  //Intro Line
+  console.log(
+    `\n         ${chalk.bold.bgWhite.hex("#a62133")(
+      "Project 04: Currency Converter CLI"
+    )}  ${chalk.hex("#c395ed").italic("By Muhammad Shahbaz\n")}`
+  );
+
+  let data = await apiService();
+  //Getting Name's Of Countries from API Data.
+  let countries = Object.keys(data);
+
+  //Base Country Selection
+  let firstCountry = await inquirer.prompt({
+    type: "list",
+    name: "name",
+    message: "Converting From :",
+    choices: countries,
+  });
+
+  // Convert Country Selection
+  let secondCountry = await inquirer.prompt({
+    type: "list",
+    name: "name",
+    message: "Converting To :",
+    choices: countries,
+  });
 };
