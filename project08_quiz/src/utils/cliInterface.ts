e48698c649e552ef56e44f2345fa8318b1ca66ed
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-02-19 11:06:00 +0500

    💫: Add Animations

--- project08_quiz/src/utils/cliInterface.ts
+++ project08_quiz/src/utils/cliInterface.ts
@@ -1,10 +1,16 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
+import ora from "ora";
 import { apiService } from "../services/apiService.js";
 import { resQuizType } from "../services/apiService.js";
 import { quiz } from "./quiz.js";
 
 export const cliInterface = async () => {
+  // Promise-based delay function for Spinner's
+  const delay = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
   let { name } = await inquirer.prompt({
     type: "input",
     name: "name",
@@ -18,10 +24,21 @@ export const cliInterface = async () => {
     choices: ["Easy", "Medium", "Hard"],
   });
 
+  //Spinner For Animations
+  const spinner = ora({
+    text: chalk.hex("#F43F5E")("Please Wait, While we Prepare the Quiz."),
+    spinner: "dots2",
+  }).start();
+
+  await delay(3000);
+
+  spinner.succeed(chalk.rgb(34, 197, 94)("QUIZ PREPARED "));
+
   //making difficulty Value to Lower Case Due to API Requirment's..
   //calling apiService
   let data: resQuizType = await apiService(difficulty.level.toLowerCase());
 
   //passing Data to Quiz Module ...
+  console.log(`\t`);
   await quiz(data);
 };
