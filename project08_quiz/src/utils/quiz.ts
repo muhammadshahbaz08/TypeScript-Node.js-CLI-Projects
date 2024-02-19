b7fdc41193240b3cd7d883a5f23e8db03f6bb5de
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-02-19 10:33:30 +0500

    ✅: Add Answer Validation in Quiz Module.

--- project08_quiz/src/utils/quiz.ts
+++ project08_quiz/src/utils/quiz.ts
@@ -1,5 +1,7 @@
 import inquirer from "inquirer";
 import { resQuizType } from "../services/apiService.js";
+import chalk from "chalk";
+import { result } from "./result.js";
 
 export const quiz = async (data: resQuizType) => {
   let score: number = 0;
@@ -16,11 +18,26 @@ export const quiz = async (data: resQuizType) => {
       data[i].correct_answer,
     ];
 
+    //displaying Questions's
     let { quizQuery } = await inquirer.prompt({
       type: "list",
       name: "quizQuery",
       message: data[i].question,
       choices: answers,
     });
-  }
+
+    // Score Calc. & Correct Answer Validation for user ..
+
+    if (quizQuery === data[i].correct_answer) {
+      ++score;
+      console.log(`\n\tYour Answer is ${chalk.bold.green(`CORRECT`)}.`);
+    } else {
+      console.log(`\n\tYour Answer is ${chalk.bold.redBright(`WRONG`)}.`);
+      console.log(
+        `\tCorrect Answer is ${chalk.green.bold(data[i].correct_answer)}\n`
+      );
+    }
+  } //loop end's
+
+  result(score);
 };
