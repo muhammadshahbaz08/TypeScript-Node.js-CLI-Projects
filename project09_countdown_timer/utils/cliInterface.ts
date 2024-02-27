775c2a9c5ddf0a8b65e8856e0d6c0cd7005375dd
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-02-27 10:27:58 +0500

    Add a Promise For Multiple time Usage of CLI

--- project09_countdown_timer/utils/cliInterface.ts
+++ project09_countdown_timer/utils/cliInterface.ts
@@ -1,53 +1,71 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 import Table from "cli-table3";
+import { endCli } from "./endCli.js";
 
 export const cliInterface = async () => {
-  const { minutes, seconds } = await inquirer.prompt([
-    {
-      type: "number",
-      name: "minutes",
-      message: "Enter the countdown duration (minutes):",
-    },
-    {
-      type: "number",
-      name: "seconds",
-      message: "Enter the countdown duration (seconds):",
-    },
-  ]);
+  console.log(
+    `\n         ${chalk.bold.bgWhite.hex("#a62133")(
+      "Project 09: CountDown Timer CLI"
+    )}  ${chalk.hex("#c395ed").italic("By Muhammad Shahbaz\n")}`
+  );
 
-  const endTime = new Date();
-  endTime.setMinutes(endTime.getMinutes() + minutes);
-  endTime.setSeconds(endTime.getSeconds() + seconds);
+  let exit = false;
+  do {
+    const { minutes, seconds } = await inquirer.prompt([
+      {
+        type: "number",
+        name: "minutes",
+        message: "Enter the countdown duration (minutes):",
+      },
+      {
+        type: "number",
+        name: "seconds",
+        message: "Enter the countdown duration (seconds):",
+      },
+    ]);
 
-  const timerInterval = setInterval(() => {
-    const currentTime = new Date();
-    const remainingTime = (endTime.getTime() - currentTime.getTime()) / 1000; // Time difference in seconds
+    const endTime = new Date();
+    endTime.setMinutes(endTime.getMinutes() + minutes);
+    endTime.setSeconds(endTime.getSeconds() + seconds);
+    //the await new Promise((resolve) => {...})  will pause the execution of the do-while loop until the Promise is resolved, which happens when the countdown ends.
+    await new Promise<void>((resolve) => {
+      const timerInterval = setInterval(() => {
+        const currentTime = new Date();
+        const remainingTime =
+          (endTime.getTime() - currentTime.getTime()) / 1000;
 
-    if (remainingTime <= 0) {
-      clearInterval(timerInterval);
-      console.log(chalk.red.bold("Countdown ended!"));
-    } else {
-      console.clear(); // Clear console to update time
+        if (remainingTime <= 0) {
+          clearInterval(timerInterval);
+          console.log(chalk.red.bold("\n\tCountdown ended!\n"));
+          console.log(
+            chalk.whiteBright(
+              `=======================================================\n`
+            )
+          );
+          resolve();
+        } else {
+          console.clear();
 
-      const remMinutes = Math.floor(remainingTime / 60);
-      const remSeconds = Math.floor(remainingTime % 60);
+          const remMinutes = Math.floor(remainingTime / 60);
+          const remSeconds = Math.floor(remainingTime % 60);
 
-      // Create a new table instance
-      const table = new Table({
-        head: [
-          chalk.rgb(245, 158, 11)("Minutes Left  "),
-          chalk.rgb(245, 158, 11)("Second's Left"),
-        ],
-        colWidths: [40, 40],
-        colAligns: ["center", "center"],
-      });
+          const table = new Table({
+            head: [
+              chalk.rgb(245, 158, 11)("Minutes Left  "),
+              chalk.rgb(245, 158, 11)("Second's Left"),
+            ],
+            colWidths: [40, 40],
+            colAligns: ["center", "center"],
+          });
 
-      // Add rows to the table
-      table.push([`${remMinutes} minutes`, `${remSeconds} seconds`]);
+          table.push([`${remMinutes} minutes`, `${remSeconds} seconds`]);
 
-      // Print the table to the console
-      console.log(table.toString());
-    }
-  }, 1000);
+          console.log(table.toString());
+        }
+      }, 1000);
+    });
+
+    exit = await endCli();
+  } while (!exit);
 };
