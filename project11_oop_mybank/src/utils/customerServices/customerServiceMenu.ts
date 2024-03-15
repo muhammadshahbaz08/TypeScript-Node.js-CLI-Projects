5294cff0d79548c833b1cfd3fe66547be3456c09
Muhammad Shahbaz <38607213+muhammadshahbaz08@users.noreply.github.com>
2024-03-15 12:04:59 +0500

    💫: Add Animations in logOut

--- project11_oop_mybank/src/utils/customerServices/customerServiceMenu.ts
+++ project11_oop_mybank/src/utils/customerServices/customerServiceMenu.ts
@@ -1,5 +1,8 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
+import ora from "ora";
+//delay module for spinner's animation
+import { delay } from "../cliInterface.js";
 import { Customer } from "../../classes/customer.js";
 import { debit } from "./debit.js";
 import { credit } from "./credit.js";
@@ -28,11 +31,15 @@ export const customerServiceMenu = async (customer: Customer) => {
     } else if (res.select === "BalanceInquiry") {
       await balanceInquiry(customer);
     } else if (res.select === "LogOut") {
-      console.log(
-        chalk.red.bold(`       
-              LogOut
-               `)
-      );
+      // Spinner For Animations
+      const spinner = ora({
+        text: chalk.hex("#F43F5E")("Logging out, please wait..."),
+        spinner: "dots2",
+      }).start();
+
+      await delay(3000);
+      spinner.succeed(chalk.hex("#E11D48").bold("Logged Out Successfully\n"));
+
       continueTransaction = false;
     }
   }
